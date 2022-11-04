8f7c2a67c6779e6a3796a2d1cdd1750ac628f199
Felix Nabucodonosor Peña <nabucopena@gmail.com>
2022-11-04 20:09:06 -0300

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  
+  root "scores#ranking"
+  post 'new_game', to: 'games#new'
+  post 'new_player', to: 'players#new'
 end
