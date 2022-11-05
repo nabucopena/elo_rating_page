c904352262f0f7335a2db4d339ee4431cecf9bf6
Felix Nabucodonosor Peña <nabucopena@gmail.com>
2022-11-05 16:19:23 -0300

    Calculate Elo

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,33 @@
 class GamesController < ApplicationController
-  def calculate_new_score(winner_score, loser_score)
-    return {winner: 1200, loser: 1200}
+  def k_factor(score, games)
+    if games < 30
+      return 40
+    else
+      if score < 2400
+        return 20
+      else
+        return 10
+      end
+    end
+  end
+
+  def expected(winner_score, loser_score)
+    return {winner: 1/(1+10**((loser_score-winner_score)/400.to_f)), loser: 1/(1+10**((winner_score-loser_score)/400.to_f))}
+  end
+
+  def calculate_new_scores(winner_score, winner_games, loser_score, loser_games)
+    winner_k = k_factor(winner_score, winner_games)
+    loser_k = k_factor(loser_score, loser_games)
+    expected_scores = expected(winner_score, loser_score)
+    puts expected_scores
+    puts loser_score + loser_k*(1 - expected_scores[:loser])
+    return {winner: winner_score + winner_k*(1 - expected_scores[:winner]), loser: loser_score + loser_k*(0 - expected_scores[:loser])}
   end
 
   def new
     winner = Score.find(params[:winner])
     loser = Score.find(params[:loser])
-    new_scores = calculate_new_score(winner.score, loser.score)
+    new_scores = calculate_new_scores(winner.score, winner.games, loser.score, loser.games)
     Score.transaction do
       begin
         winner.update(score: new_scores[:winner], games: winner.games + 1)
